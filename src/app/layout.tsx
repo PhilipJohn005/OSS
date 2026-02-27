9b7544ac72a7b6042be10e25de7dabd007a25df5
PhilipJohn005 <pjdeluge123@gmail.com>
2026-02-27 16:10:15 +0530

    Feat: add clarity tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,18 @@
 import './globals.css'
 import { SessionProvider } from "next-auth/react";
 import 'highlight.js/styles/vs2015.css';
-import Script from "next/script";
 import { ReactNode } from 'react';
+import MicrosoftClarity from "./metrics/MicrosoftClarity";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html>
       <body>
-
-        <Script
-          id="clarity"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(c,l,a,r,i,t,y){
-                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "vntevgong6");
-            `,
-          }}
-        />
-
         <SessionProvider>
           {children}
         </SessionProvider>
 
+        <MicrosoftClarity />
       </body>
     </html>
   );
